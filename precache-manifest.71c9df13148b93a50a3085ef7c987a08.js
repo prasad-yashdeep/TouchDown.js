d570fc6f00e581a68807416f841ba32b20446fce
HackGod2000 <45161513+HackGod2000@users.noreply.github.com>
2020-07-23 00:11:12 +0530

    Updates

---
+++ precache-manifest.71c9df13148b93a50a3085ef7c987a08.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146961fc4213a90bf29b6f7cef40fd35",
+    "url": "/TouchDown.js/index.html"
+  },
+  {
+    "revision": "fcf41e863617f597c585",
+    "url": "/TouchDown.js/static/css/main.35d23f74.chunk.css"
+  },
+  {
+    "revision": "10cd9c14b007e70e753a",
+    "url": "/TouchDown.js/static/js/2.2ca723b1.chunk.js"
+  },
+  {
+    "revision": "7e7bbff4e5462d41d81ae3e6013b9896",
+    "url": "/TouchDown.js/static/js/2.2ca723b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcf41e863617f597c585",
+    "url": "/TouchDown.js/static/js/main.b58d48f5.chunk.js"
+  },
+  {
+    "revision": "eea81577bf981042f67b",
+    "url": "/TouchDown.js/static/js/runtime-main.0adbce8e.js"
+  },
+  {
+    "revision": "ae5e05e218040eb48f8ab74c5e23ff38",
+    "url": "/TouchDown.js/static/media/background.ae5e05e2.png"
+  },
+  {
+    "revision": "bc00de04016ed75c1dce6fc13e134b5f",
+    "url": "/TouchDown.js/static/media/brick_wall.bc00de04.jpg"
+  },
+  {
+    "revision": "f68f2bda183db9bda046f6530bf2de1e",
+    "url": "/TouchDown.js/static/media/marsbump1k.f68f2bda.jpg"
+  },
+  {
+    "revision": "56f226a559fd3807a5aab2e2efab8e24",
+    "url": "/TouchDown.js/static/media/marsmap1k.56f226a5.jpg"
+  },
+  {
+    "revision": "08608e7c978e04269235abd51042cae0",
+    "url": "/TouchDown.js/static/media/weighted_wall.08608e7c.jpg"
+  }
+]);
